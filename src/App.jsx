8fead49568e0979ee479efbf1e2c3e785589859e
thesoorya <sooryanarayanansrinivasan@gmail.com>
2024-05-20 14:29:06 +0530

    basic setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import State from './components/State'
+import Effect from './components/Effect'
+
+function App() {
+
+  return (
+    <>
+     {/* <State /> */}
+     <Effect />
+    </>
+  )
+}
+
+export default App
